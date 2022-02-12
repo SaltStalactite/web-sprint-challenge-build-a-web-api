5d0279977817727b968ca9018e33ef6a0c4cce20
cc <votaryofturms@icloud.com>
2022-02-12 02:18:08 -0500

    actions router get

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,29 @@
-// add middlewares here related to actions
+const Action = require('./actions-model')
+
+function checkBody(req, res, next) {
+    if (!req.body.name || !req.body.description) {
+        next({ status: 400, message: 'Invalid request' })
+    } else {
+        next()
+    }
+}
+
+async function idExists(req, res, next) {
+    const { id } = req.params;
+    const dbId = await Action.get(id)
+    try {
+        if (!dbId) {
+            next({ status: 404, message: 'ID is not found' })
+        } else {
+            req.id = dbId
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    checkBody,
+    idExists
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,4 +3,18 @@ const express = require('express');
 
 const router = express.Router()
 
+const Action = require('./actions-model')
+
+const {
+    checkBody,
+} = require('./actions-middlware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        res.json(await Action.get())
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router;
