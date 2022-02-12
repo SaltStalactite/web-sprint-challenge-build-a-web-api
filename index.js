b3cbc16d789b48bea64133d61ecc3bd517720030
cc <votaryofturms@icloud.com>
2022-02-11 19:44:23 -0500

    routers

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require('./api/server')
 
+require('dotenv').config();
+
+const server = require('./api/server');
+
 const PORT = process.env.PORT || 9008;
 
 server.listen(PORT, () => {
