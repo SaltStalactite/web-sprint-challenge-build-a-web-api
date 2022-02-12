e493edb18b77d594c6e97502e53fa517073e308a
cc <votaryofturms@icloud.com>
2022-02-11 19:47:50 -0500

    projectrouter get

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const server = require('./api/server')
 
 require('dotenv').config();
 
-const server = require('./api/server');
-
 const PORT = process.env.PORT || 9008;
 
 server.listen(PORT, () => {
